c54bc22444960f33e820989cceb2721682e3f540
Bahey el Din Ismail <90637888+Bahey1200022@users.noreply.github.com>
2022-12-21 20:06:13 +0200

    ok

--- main.py
+++ main.py
@@ -21,22 +21,27 @@ def home():
 
    
 
-# @app.route('/loginDr',methods =['POST','GET'])
-# def loginDr():
-#     if request.method =='POST':
-#         n=request.form['x']
-#         d=request.form['y']
-#         sql="SELECT * FROM doctors WHERE (email=%s AND password =%s)"
-#         val=(n,d)
-#         mycursor.execute(sql, val)
-#         r=mycursor.fetchone()
+@app.route('/loginDr',methods =['POST','GET'])
+def loginDr():
+    if request.method =='POST':
+        n=request.form['x']
+        d=request.form['y']
+        sql="SELECT * FROM doctors WHERE (email=%s AND password =%s)"
+        val=(n,d)
+        mycursor.execute(sql, val)
+        r=mycursor.fetchone()
+        if r==None:
+          return render_template('doctorslogin.html')
+          
+        
+        
          
         
         
         
-#         return render_template('doctorsaddorview.html',data=r)
-#     else :
-#       return render_template('doctorslogin.html')    
+        return render_template('doctorsaddorview.html',data=r)
+    else :
+      return render_template('doctorslogin.html')    
         
 
 
