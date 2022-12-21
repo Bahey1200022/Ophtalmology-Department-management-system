6a984cc9c01a855988488c822c9f85188857ff2f
Bahey el Din Ismail <90637888+Bahey1200022@users.noreply.github.com>
2022-12-21 19:19:43 +0200

    Fuck flask

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, flash, request, url_for, redirect, session
+import mysql.connector
+
+
+
+
 app=Flask(__name__)
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="palmhome",
+  database="optha"
+)
+
+mycursor = mydb.cursor()
+
+@app.route('/')
+def home():
+   return render_template('p1.html')
+ 
+
+   
+
+# @app.route('/loginDr',methods =['POST','GET'])
+# def loginDr():
+#     if request.method =='POST':
+#         n=request.form['x']
+#         d=request.form['y']
+#         sql="SELECT * FROM doctors WHERE (email=%s AND password =%s)"
+#         val=(n,d)
+#         mycursor.execute(sql, val)
+#         r=mycursor.fetchone()
+         
+        
+        
+        
+#         return render_template('doctorsaddorview.html',data=r)
+#     else :
+#       return render_template('doctorslogin.html')    
+        
+
 
 if __name__=='__main__':
     app.run()
